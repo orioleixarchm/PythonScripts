93ad699c4e9102b55e94b33ea1d65429a91063e1
orioleixarchm <133284998+orioleixarchm@users.noreply.github.com>
2025-07-21 15:30:03 +0200

    Update Infra_risk_failure.py

--- Econometrics/Infra_risk_failure.py
+++ Econometrics/Infra_risk_failure.py
@@ -11,14 +11,14 @@ Size_K = 3
 Sample_Size = 100000
 Budget = 100000
 
-#Weibul (failure) and Log-Normal (Costs) distributions
+#Weibul Distribution
 T = np.random.weibull(a=Size_K,size=Sample_Size)*Lambda
 lognormal_costs = np.random.lognormal(mean=11, sigma=0.8,size=Sample_Size)
-fig_I, axes_I = plt.subplots(2,1)
+fig_I, axes_I = plt.subplots(1,2)
 fig_I.tight_layout()
 sns.histplot(T, kde=True, ax=axes_I[0], edgecolor='black',linewidth=0.75)
 axes_I[0].set_title(f'Weibul Distribution with K={Size_K} and Lambda={Lambda}.')
-sns.histplot(lognormal_costs, kde=True, ax=axes_I[1], color='red', edgecolor='black',linewidth=0.75, bins=45)
+sns.histplot(lognormal_costs, kde=True, ax=axes_I[1], color='red', edgecolor='black',linewidth=0.75, bins=65)
 axes_I[1].set_title(f'Log Normal Distribution with mean={11} and sigma={0.4}.')
 
 #Prob of failure based on threshholds
@@ -39,7 +39,7 @@ for thr, axA, axB in zip(min_years,axes_II,axes_III):
     expected_cost = np.mean(costs)
     prob_over_budget = np.mean(costs > Budget)
     print(f"For a min threshold of {thr} years the probability is {round(prob*100,2)}%; analytically it is {round(prob_exact*100,2)} (difference of {round(prob-prob_exact,4)})")
-    print(f"For a budget of {Budget}€, the expected costs are {expected_cost}€, the probability of exceeding is {round(prob_over_budget*100,2)}%.")
+    print(f"For a budget of {Budget}€, the expected costs are {expected_cost}€, the probability of exceeding is {round(prob_over_budget*100,2)}%.",'\n')
     sns.histplot(T < thr, kde=True, ax=axA, edgecolor='black',linewidth=0.75)
     axA.axvline(prob,color='red')
     axA.set_ylabel(None)
